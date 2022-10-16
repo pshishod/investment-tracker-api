0812eac26fdefd6d4185bc39d07c80d6d9e58817
Priyansh Shishodia <priyanshshishodia@gmail.com>
2022-10-16 17:50:59 -0500

    Initial commit

---
+++ src/main/java/com/priyansh/investmenttrackerapi/InvstmentTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.priyansh.investmenttrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvstmentTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvstmentTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyansh/investmenttrackerapi/InvstmentTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyansh.investmenttrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvstmentTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
